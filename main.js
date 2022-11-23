4ec5d32945dbbf7499b723f4f08804b1b10cffaa
Felipe Araujo <araujo.felipedev@gmail.com>
2022-11-23 20:17:24 -0300

    destination nav js functions

--- main.js
+++ main.js
@@ -9,3 +9,29 @@ menuHamburger.addEventListener("click", ()=>{
 })
 
 
+/*Funcionalidade do nav da aba 'Destination'*/
+
+const navBtn = document.querySelectorAll('.destination__nav__item')
+const planetas = document.querySelectorAll('.planeta')
+
+navBtn.forEach((arrayElement)=>{ // Função executada em cada botão do nav
+   arrayElement.addEventListener("click", (evento)=>{
+      addClasses(evento.target.innerHTML) // esconde todas as sections e mostra somente a equivalente ao item clicado
+      removeClass() // remove a class que coloca o sublinhado no item
+      arrayElement.classList.add('destination__nav__item-ativo') // adiciona a class que sublinha o item
+   })
+})
+
+function addClasses(planeta){
+   planetas.forEach((arrayElement)=>{
+      arrayElement.classList.add('hidden')  
+   })
+   mostrar = document.querySelector('.'+planeta)
+   mostrar.classList.remove('hidden')
+}
+
+function removeClass() {
+   navBtn.forEach((arrayElement)=>{    
+      arrayElement.classList.remove('destination__nav__item-ativo')
+   })
+}
