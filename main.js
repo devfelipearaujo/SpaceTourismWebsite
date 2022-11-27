346677422108975717e2a3ef1fb82ab669e1e450
Felipe Araujo <araujo.felipedev@gmail.com>
2022-11-27 07:31:35 -0300

    crew page html, css & js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*Funcionalidade do Menu Hamburger*/
+/* ------ Funcionalidade do Menu Hamburger*/
 
 const menuHamburger = document.querySelector('.menu__hamburger')
 
@@ -9,10 +9,10 @@ menuHamburger.addEventListener("click", ()=>{
 })
 
 
-/*Funcionalidade do nav da aba 'Destination'*/
+/* ------ Funcionalidade do nav da aba 'Destination'*/
 
 const navBtn = document.querySelectorAll('.destination__nav__item')
-const planetas = document.querySelectorAll('.planeta')
+const planetas = document.querySelectorAll('[data-planeta]')
 
 navBtn.forEach((arrayElement)=>{ // Função executada em cada botão do nav
    arrayElement.addEventListener("click", (evento)=>{
@@ -35,3 +35,37 @@ function removeClass() {
       arrayElement.classList.remove('destination__nav__item-ativo')
    })
 }
+
+/* ------ Funcionalidade do carrossel da aba 'Crew' */
+
+const paginationBullet = document.querySelectorAll('.swiper-pagination-bullet')
+const sectionInfo = document.querySelectorAll('.section__info')
+const slide = document.querySelector('.crew')
+
+slide.addEventListener("mousemove", ()=>{
+   changeSlideText()
+})
+
+slide.addEventListener("touchend", ()=>{
+   changeSlideText()
+})
+
+function changeSlideText(){
+   let i = 0;
+   paginationBullet.forEach((arrayElement)=>{
+      if (arrayElement.classList.value == 'swiper-pagination-bullet swiper-pagination-bullet-active'){
+         sectionInfo[i].classList.remove('hidden')
+         /* console.log(i) */
+      } else {
+         sectionInfo[i].classList.add('hidden')
+      }
+      i = i + 1;
+   })
+}
+
+
+
+
+
+
+
