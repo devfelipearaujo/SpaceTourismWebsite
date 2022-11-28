0fdf17261a7ad2f14af949c187842bd96a16e678
Felipe Araujo <araujo.felipedev@gmail.com>
2022-11-28 08:57:08 -0300

    images in destination section now change to follow the iformation text

--- technology.js
+++ technology.js
@@ -1,5 +1,6 @@
 const techBtn = document.querySelectorAll('.tech__nav__item')
 const sectionInfo = document.querySelectorAll('.section__info')
+const techImagem = document.querySelectorAll('.tech__imagem')
 
 techBtn.forEach((arrayElement)=>{
    arrayElement.addEventListener("click", (evento)=>{
@@ -14,9 +15,11 @@ function trocarTexto(){
    techBtn.forEach((arrayElement)=>{   
       if(arrayElement.classList.value == 'tech__nav__item tech__nav__item-ativo'){
          sectionInfo[i].classList.remove('hidden')
+         techImagem[i].classList.remove('hidden')
          console.log(i)
       } else {
          sectionInfo[i].classList.add('hidden')
+         techImagem[i].classList.add('hidden')
       }
       i = i + 1;
    })
